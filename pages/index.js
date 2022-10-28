1993353efc16b02280d56a672d83f41dbc696558
Mortimore316 <kclittlered12@gmail.com>
2022-10-27 22:20:06 -0500

    Testing Axios on Events

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,27 @@
 import Head from 'next/head'
+import React from 'react';
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import axios from 'axios';
+
 
 export default function Home() {
+  const [events, setEvents] = React.useState([]);
+
+  React.useEffect(() => {
+    GetEvents();
+  }, [])
+
+  const GetEvents = async () => {
+    try {
+      const response = await axios.get('http://localhost:3000/events')
+      const data = response.data
+      setEvents(data.reverse());
+      console.log(events);
+    } catch (e) {
+      console.log(e);
+    }
+  };
   return (
     <h1 className="text-3xl font-bold underline">
       Hello world!
